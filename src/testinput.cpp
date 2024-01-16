a3851abe86a75bc17d8986119afba898257a5ba2
ThaKhinso <usersoe123@gmail.com>
2024-01-16 18:26:14 +0630

    bla

---
+++ src/testinput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+c
+
+int main() {
+    std::vector<int>crap;
+    crap.reserve(3);
+    crap.push_back(1);
+    crap[2] = 3;
+    {
+        crap[0] = 123;
+    }
+    std::cout << crap[0] << "\n";
+    
+}
